78d226e1af77bbb293c7c9969b0fda5ac0cc29d8
Konstantin_Korostelev <Konstantin_Korostelev@epam.com>
2018-12-13 10:47:05 +0400

    final ver 0.91

--- src/main/java/task01/Runner.java
+++ src/main/java/task01/Runner.java
@@ -1,15 +1,10 @@
 package task01;
 
-import com.mashape.unirest.http.Unirest;
-
-import java.io.IOException;
-
 import static task01.YandexMapsCoordinatesRequester.getCoordinates;
 
 public class Runner {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         getCoordinates("Ижевск 40 лет Победы 142 41");
         getCoordinates("EPAM Minsk");
-        Unirest.shutdown();
     }
 }
